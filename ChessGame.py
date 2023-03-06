64abd3a1c99a5cb088c228b17115c0d4ae07af1e
Stam21 <george.stamatopoulos1998@gmail.com>
2023-03-06 04:50:08 +0200

    add legal moves

--- ChessGame.py
+++ ChessGame.py
@@ -3,18 +3,43 @@ This class stores all the information of the current state about the chess game,
 information about the validity of moves.
 """
 import numpy as np
+import Actions
+import chess
 
 class GameState():
     def __init__(self):
         self.board = [
             np.array(["bR","bN","bB","bQ","bK","bB","bN","bR"]),
             np.array(["bp","bp","bp","bp","bp","bp","bp","bp"]),
-            np.array([" "," "," "," "," "," "," "," "]),
-            np.array([" "," "," "," "," "," "," "," "]),
-            np.array([" "," "," "," "," "," "," "," "]),
-            np.array([" "," "," "," "," "," "," "," "]),
+            np.array(["--","--","--","--","--","--","--","--"]),
+            np.array(["--","--","--","--","--","--","--","--"]),
+            np.array(["--","--","--","--","--","--","--","--"]),
+            np.array(["--","--","--","--","--","--","--","--"]),
             np.array(["wp","wp","wp","wp","wp","wp","wp","wp"]),
             np.array(["wR","wN","wB","wK","wQ","wB","wN","wR"])
         ]
         self.whiteMove = True
+        self.blackKing = (0,4)
+        self.whiteKing = (7,3)
+        self.boardHelper = chess.Board( 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBKQBNR')
+
+    
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol]= "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        if (move.pieceMoved == "wK"):
+            self.whiteKing  = (move.endRow, move.endCol)
+        elif (move.pieceMoved == "bK"):
+            self.blackKing = (move.endRow, move.endCol)
+
+        self.boardHelper.push(chess.Move.from_uci(move.getChessNotation()))
+        self.whiteMove = not self.whiteMove 
         
+
+    def getValidMoves(self):
+
+        self.moves = []
+        for move in self.boardHelper.legal_moves:
+            self.moves.append(move)
+        
+        return self.moves
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Main will be responsible of displaying the graphics and current chessengine obje
 
 import pygame as pg
 import ChessGame
+import Actions
 
 WIDTH = HEIGHT = 768
 DIMENSION = 8
@@ -13,18 +14,9 @@ IMAGES = {}
 pg.init()
 
 def loadImages():
-    IMAGES['wp'] = pg.transform.scale(pg.image.load("images/wp.png"), (SQUARE_SIZE,SQUARE_SIZE))
-    IMAGES['wR'] = pg.transform.scale(pg.image.load("images/wR.png"), (SQUARE_SIZE,SQUARE_SIZE))
-    IMAGES['wB'] = pg.transform.scale(pg.image.load("images/wB.png"), (SQUARE_SIZE,SQUARE_SIZE))
-    IMAGES['wK'] = pg.transform.scale(pg.image.load("images/wK.png"), (SQUARE_SIZE,SQUARE_SIZE))
-    IMAGES['wQ'] = pg.transform.scale(pg.image.load("images/wQ.png"), (SQUARE_SIZE,SQUARE_SIZE))
-    IMAGES['wN'] = pg.transform.scale(pg.image.load("images/wN.png"), (SQUARE_SIZE,SQUARE_SIZE))
-    IMAGES['bp'] = pg.transform.scale(pg.image.load("images/bp.png"), (SQUARE_SIZE,SQUARE_SIZE))
-    IMAGES['bR'] = pg.transform.scale(pg.image.load("images/bR.png"), (SQUARE_SIZE,SQUARE_SIZE))
-    IMAGES['bB'] = pg.transform.scale(pg.image.load("images/bB.png"), (SQUARE_SIZE,SQUARE_SIZE))
-    IMAGES['bK'] = pg.transform.scale(pg.image.load("images/bK.png"), (SQUARE_SIZE,SQUARE_SIZE))
-    IMAGES['bQ'] = pg.transform.scale(pg.image.load("images/bQ.png"), (SQUARE_SIZE,SQUARE_SIZE))
-    IMAGES['bN'] = pg.transform.scale(pg.image.load("images/bN.png"), (SQUARE_SIZE,SQUARE_SIZE))
+    gallery = ['wp','wR','wB','wK','wQ','wN','bp','bR','bB','bK','bQ','bN']
+    for pic in gallery:
+        IMAGES[pic] = pg.transform.scale(pg.image.load("images/" + pic + ".png"), (SQUARE_SIZE,SQUARE_SIZE))
 
 
 def drawBoard(display):
@@ -40,7 +32,7 @@ def drawPieces(display,board):
      for x in range(DIMENSION):
         for y in range(DIMENSION):
             piece = board[x][y]
-            if piece != " ":
+            if piece != "--":
                 display.blit(IMAGES[piece],pg.Rect(y*SQUARE_SIZE,x*SQUARE_SIZE, SQUARE_SIZE,SQUARE_SIZE))
 
 
@@ -54,10 +46,41 @@ def main():
     loadImages()
     drawBoard(display)
     drawPieces(display,game_state.board)
+    square = () # tuple that stores the current selected square
+    piecePositions = [] # starting position and destination
+    validMoves = game_state.getValidMoves()
+    moved = False
     while playingMode:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 playingMode = False
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                mouse_position = pg.mouse.get_pos()
+                mouse_x = mouse_position[0]//SQUARE_SIZE
+                mouse_y = mouse_position[1]//SQUARE_SIZE
+                # check if same square is selected twice
+                if (square == (mouse_y,mouse_x)):
+                    square = ()
+                    piecePositions =[]
+                else:
+                    square = (mouse_y,mouse_x)
+                    piecePositions.append(square)
+                if len(piecePositions) == 2:
+                    move  = Actions.Move(piecePositions[0],piecePositions[1],game_state.board)
+                    for vm in validMoves:
+                        if (str(move.getChessNotation()) == str(vm)):
+                            game_state.makeMove(move)
+                            moved = True
+
+                    square = ()
+                    piecePositions =[]
+
+        if moved:
+            validMoves = game_state.getValidMoves()
+            moved = False
+
+        drawBoard(display)
+        drawPieces(display, game_state.board) # piece highlighting or move suggestions can be added later on here
         clock.tick(MAX_FPS)
         pg.display.flip()
 
