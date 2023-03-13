49e7f856513e3aa12f68b3d9367394f6fa5e7b7b
MarioRojoVicente <estudiantedinamo@gmail.com>
2023-03-13 21:02:24 +0100

    Create adaptationBackboneToGUI.py

---
+++ adaptationBackboneToGUI.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 13 11:00:04 2023
+
+@author: Mario
+"""
+import numpy as np
+
+NUMBERS_TO_STRINGS = {
+    0 : "--",
+    1 : "wp",
+    2 : "wN",
+    3 : "wB",
+    4 : "wR",
+    5 : "wQ",
+    6 : "wK",
+    7 : "bp",
+    8 : "bN",
+    9 : "bB",
+    10 : "bR",
+    11 : "bQ",
+    12 : "bK",
+    
+    }
+
+def convertToStrings(matrix):
+    copy = matrix
+    for x in range(len(matrix)):
+        for y in range(len(matrix[0])):
+            copy[x][y] = NUMBERS_TO_STRINGS[matrix[x][y]]
+        
+        copy[x] = np.asarray(copy[x])
+    
+    return copy
+
+m = [[1,2,3],[4,5,6],[0,7,8]]
+print(convertToStrings(m))
+    
