3fd062ee0fcbf6d89bada6b6b3b9ef7f37b72ba9
MarioRojoVicente <estudiantedinamo@gmail.com>
2023-03-14 11:08:03 +0100

    Create utlity_3.py

---
+++ utlity_3.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 14 09:52:05 2023
+
+@author: Mario
+"""
+import math as mt
+
+
+tempBoard = [[10,8,9,11,12,9,8,10],
+             [7,7,7,7,7,7,7,7],
+             [0,0,0,0,0,0,0,0],
+             [1,1,1,1,1,1,1,1],
+             [1,1,1,1,1,1,1,1],
+             [0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0]]
+
+def getDistance(pos, center):
+    return mt.sqrt(((pos[0] - center[0])**2) + ((pos[1] - center[1])**2))
+
+def utility_3(board, isWhite):
+    whites = 0
+    blacks = 0
+    cw = 0
+    cb = 0
+    for x in range(len(board)):
+        for y in range(len(board[0])):
+            if (int((board[x][y]-1)/6) == 0 and board[x][y]!=0):
+                cw = cw+1
+                whites = whites + getDistance((x,y),((len(board)-1)/2,(len(board[0])-1)/2))
+            elif (board[x][y]!=0):
+                cb = cb+1
+                blacks = blacks + getDistance((x,y),((len(board)-1)/2,(len(board[0])-1)/2))
+    
+    retval = 0
+    if (isWhite):
+        retval = whites - blacks
+    else:
+        retval = blacks - whites
+    
+    print(cw, cb)
+    print("w", whites/cw)
+    print("b", blacks/cb)
+    return retval/5
+            
+
+
+utility_3(tempBoard, True)
+    
