c302a2c0d3170d2d9050e996312d7c29681836ce
MarioRojoVicente <estudiantedinamo@gmail.com>
2023-03-18 11:49:54 +0100

    final corrections to prunning

--- pruning.py
+++ pruning.py
@@ -18,14 +18,17 @@ def utility_2(board):
 def utility_3(board):
     return 1
 ################################################
+
+NUMBER = []
 # Alpha-beta pruning used to optimize the minimax algorithm by reducenumber of nodes required to be evulated
 #add two argument(alpha= best value by maximizing player, beta= best value for minimizing player)
-def getNodeValue(board, isWhite, depth, alpha, beta, next_move, parent= None): 
+def getNodeValue(board, isWhite, depth, alpha, beta, next_move):
+    NUMBER.append(1)
     #terminal codition by depth
     if(depth == MAX_DEPTH):
         
         boardValue = W1*utility_1(board) + W2*utility_2(board) + W3*utility_3(board)
-        return boardValue, next_move, parent
+        return boardValue, next_move, alpha, beta
     
     game_table = ChessGame.GameState()
     str_board = BtG.convertToStrings(board) #convert board
@@ -40,12 +43,12 @@ def getNodeValue(board, isWhite, depth, alpha, beta, next_move, parent= None):
     #terminal condition by the rules
     if (len(moves) == 0):
         if (len(game_table.threats) == 0):
-            return 0, next_move
+            return 0, next_move, alpha, beta
         else:
             if (game_table.whiteMove == isWhite):
-                return inf, next_move
+                return inf, next_move, alpha, beta
             else:
-                return -inf, next_move
+                return -inf, next_move, alpha, beta
             
         
     #intermediate node
@@ -63,13 +66,17 @@ def getNodeValue(board, isWhite, depth, alpha, beta, next_move, parent= None):
         for child in childrenNodes:  
             # goes deeper into the tree to get the values that come to this node
             # goes thorugh every child node, compute its value in (recurisvely)call to 'getNodeValue'
-            value = getNodeValue(child, isWhite, depth + 1, alpha, beta, False, parent=(nodeValue, child_index))[0]
+            call = getNodeValue(child, isWhite, depth + 1, alpha, beta, False)
+            value = call[0]
+            alpha = max(call[3], alpha) #beta propagates up as alpha
+            beta = min(call[2], alpha)  #alpha propagates up as beta
             nodeValue = max(nodeValue, value)
             if nodeValue == value:
                 next_move_idx = child_index
             # if the value > current 'nodeValue' then algorthim update'nodeValue'
             alpha = max(alpha, nodeValue) #algorthim also update 'alpha' to maxium its current value and 'nodeValue'
-            if nodeValue >= beta:   
+            if nodeValue >= beta:
+                print("betacut")
                 # if 'v' >= 'beta', means the current node will delet, 
                 # and algorithm prune the rest nodes in tree, this done by 'break'
                 break
@@ -77,25 +84,30 @@ def getNodeValue(board, isWhite, depth, alpha, beta, next_move, parent= None):
 
     else: #min's turn
         nodeValue = inf # turn 'nodeValue' into postive infinity
-        for child in childrenNodes:  # goes deeper into the tree to get the values that come to this node
-            value = getNodeValue(child, isWhite, depth + 1, alpha, beta, True, parent=(nodeValue, child_index))[0] #go through each child node
+        for child in childrenNodes:  # goes deeper into the tree to get the values that come to this node 
+            #go through each child node
+            call = getNodeValue(child, isWhite, depth + 1, alpha, beta, True)
+            value = call[0]
+            alpha = max(call[3], alpha) #beta propagates up as alpha
+            beta = min(call[2], alpha)  #alpha propagates up as beta
             nodeValue = min(nodeValue, value) #compute its value using recurisive call to 'getNode' above 
             if nodeValue == value:
                 next_move_idx = child_index
             beta = min(beta, nodeValue)
             if nodeValue <= alpha:
+                print("alfacut")
                 break
             child_index += 1
-    print(alpha)
-    print(beta)
+    print("alpha:", alpha)
+    print("beta:", beta)
     if depth == 0:
         next_move = moves[next_move_idx]
 
-    return nodeValue, next_move, parent
+    return nodeValue, next_move, alpha, beta
 
 def getNextMove(board, isWhite):
     next_move = "NOT YET CALCULATED"
-    next_move = getNodeValue(board, isWhite, 0, inf, -inf, next_move)[1]
+    next_move = getNodeValue(board, isWhite, 0, -inf, inf, next_move)[1]
     return next_move
 
 initialBoard = [[10,8,9,11,12,9,8,10],
