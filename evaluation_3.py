ea3b6a9e093bdeec4c27ec9c0d390957976cd177
MarioRojoVicente <estudiantedinamo@gmail.com>
2023-03-19 20:40:18 +0100

    new evaluation function working version

--- evaluation_3.py
+++ evaluation_3.py
@@ -4,46 +4,113 @@ Created on Tue Mar 14 09:52:05 2023
 
 @author: Mario
 """
-import math as mt
+
+""" code based on https://www.chessprogramming.org/Simplified_Evaluation_Function"""
 
 
-test_board = [[10,8,9,11,12,9,8,10],
-             [7,7,7,7,7,7,7,7],
-             [0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0],
-             [1,1,1,1,1,1,1,1],
-             [4,2,3,5,6,3,2,4]]
+PIECE_BOARDS = {
+    1: [[0,  0,  0,  0,  0,  0,  0,  0],
+        [50, 50, 50, 50, 50, 50, 50, 50],
+        [10, 10, 20, 30, 30, 20, 10, 10],
+        [5,  5, 10, 25, 25, 10,  5,  5],
+        [0,  0,  0, 20, 20,  0,  0,  0],
+        [5, -5,-10,  0,  0,-10, -5,  5],
+        [5, 10, 10,-20,-20, 10, 10,  5],
+        [0,  0,  0,  0,  0,  0,  0,  0]],
+    
+    2: [[-50,-40,-30,-30,-30,-30,-40,-50],
+        [-40,-20,  0,  0,  0,  0,-20,-40],
+        [-30,  0, 10, 15, 15, 10,  0,-30],
+        [-30,  5, 15, 20, 20, 15,  5,-30],
+        [-30,  0, 15, 20, 20, 15,  0,-30],
+        [-30,  5, 10, 15, 15, 10,  5,-30],
+        [-40,-20,  0,  5,  5,  0,-20,-40],
+        [-50,-40,-30,-30,-30,-30,-40,-50]],
+    
+    3: [[-20,-10,-10,-10,-10,-10,-10,-20],
+        [-10,  0,  0,  0,  0,  0,  0,-10],
+        [-10,  0,  5, 10, 10,  5,  0,-10],
+        [-10,  5,  5, 10, 10,  5,  5,-10],
+        [-10,  0, 10, 10, 10, 10,  0,-10],
+        [-10, 10, 10, 10, 10, 10, 10,-10],
+        [-10,  5,  0,  0,  0,  0,  5,-10],
+        [-20,-10,-10,-10,-10,-10,-10,-20]],
+    
+    4: [[ 0,  0,  0,  0,  0,  0,  0,  0],
+        [ 5, 10, 10, 10, 10, 10, 10,  5],
+        [-5,  0,  0,  0,  0,  0,  0, -5],
+        [-5,  0,  0,  0,  0,  0,  0, -5],
+        [-5,  0,  0,  0,  0,  0,  0, -5],
+        [-5,  0,  0,  0,  0,  0,  0, -5],
+        [-5,  0,  0,  0,  0,  0,  0, -5],
+        [ 0,  0,  0,  5,  5,  0,  0,  0]],
+    
+    5: [[-20,-10,-10, -5, -5,-10,-10,-20],
+        [-10,  0,  0,  0,  0,  0,  0,-10],
+        [-10,  0,  5,  5,  5,  5,  0,-10],
+        [ -5,  0,  5,  5,  5,  5,  0, -5],
+        [ 0,  0,  5,  5,  5,  5,  0, -5],
+        [-10,  5,  5,  5,  5,  5,  0,-10],
+        [-10,  0,  5,  0,  0,  0,  0,-10],
+        [-20,-10,-10, -5, -5,-10,-10,-20]],
+    
+    0: [[[-30,-40,-40,-50,-50,-40,-40,-30],
+        [-30,-40,-40,-50,-50,-40,-40,-30],
+        [-30,-40,-40,-50,-50,-40,-40,-30],
+        [-30,-40,-40,-50,-50,-40,-40,-30],
+        [-20,-30,-30,-40,-40,-30,-30,-20],
+        [-10,-20,-20,-20,-20,-20,-20,-10],
+        [ 20, 20,  0,  0,  0,  0, 20, 20],
+        [ 20, 30, 10,  0,  0, 10, 30, 20]],
+    
+        [[-30,-40,-40,-50,-50,-40,-40,-30],
+        [-30,-40,-40,-50,-50,-40,-40,-30],
+        [-30,-40,-40,-50,-50,-40,-40,-30],
+        [-30,-40,-40,-50,-50,-40,-40,-30],
+        [-20,-30,-30,-40,-40,-30,-30,-20],
+        [-10,-20,-20,-20,-20,-20,-20,-10],
+        [20, 20,  0,  0,  0,  0, 20, 20],
+        [20, 30, 10,  0,  0, 10, 30, 20]]]
+    }
 
-def getDistance(pos, center):
-    return mt.sqrt(((pos[0] - center[0])**2) + ((pos[1] - center[1])**2))
+test_board = [[10, 8, 9,11,12, 9, 8,10],
+              [ 7, 7, 7, 7, 7, 7, 7, 7],
+              [ 0, 0, 0, 0, 0, 0, 0, 0],
+              [ 0, 0, 0, 0, 0, 0, 0, 0],
+              [ 0, 0, 0, 0, 0, 0, 0, 0],
+              [ 0, 0, 0, 0, 0, 0, 0, 0],
+              [ 1, 1, 1, 1, 1, 1, 1, 1],
+              [ 4, 2, 3, 5, 6, 3, 2, 4]]
 
+# this function will return 0 if we consider it is the beginning of the game and
+# 1 if we consider to be on the endgame
+def getGameStage(board,isWhite):
+    return 0
 
-# this evaluation function takes into consideration the average distance of all
-# the pieces of each player to the center of the board.
+# this function assigns a value to all pieces based on their position on the board
 def evaluation_3(board, isWhite):
     whites = 0
     blacks = 0
-    cw = 0
-    cb = 0
     for x in range(len(board)):
-        for y in range(len(board[0])):
-            if (int((board[x][y]-1)/6) == 0 and board[x][y]!=0):
-                cw = cw+1
-                whites = whites + getDistance((x,y),((len(board)-1)/2,(len(board[0])-1)/2))
-            elif (board[x][y]!=0):
-                cb = cb+1
-                blacks = blacks + getDistance((x,y),((len(board)-1)/2,(len(board[0])-1)/2))
-    retval = 0
-    whites = 5 - whites/cw
-    blacks = 5 - blacks/cb
+        for y in range(len(board[x])):
+            if board[x][y] != 0: 
+                if (int((board[x][y]-1)/6) == 0):
+                    if (board[x][y]%6 != 0):
+                        whites = whites + PIECE_BOARDS[board[x][y]%6][7-x][7-y]
+                    else:
+                        whites = whites + PIECE_BOARDS[0][getGameStage(board,isWhite)][7-x][7-y]
+                else:
+                    if (board[x][y]%6 != 0):
+                        blacks = blacks + PIECE_BOARDS[board[x][y]%6][x][y]
+                    else:
+                        blacks = blacks + PIECE_BOARDS[0][getGameStage(board,isWhite)][x][y]
     if (isWhite):
         retval = whites - blacks
     else:
         retval = blacks - whites
-    return retval/5
+    return retval/500
             
+#print(evaluation_3(test_board, False))
 
 
 #evaluation_3(test_board, True)
